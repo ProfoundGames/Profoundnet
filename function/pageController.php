006907cbd17606fa9746e263ebd127e11c85f302
ianno1993 <i.lol@hotmail.com>
2015-03-21 10:51:48 +0100

    Quick fix?

--- function/pageController.php
+++ function/pageController.php
@@ -1,15 +1,23 @@
 <?php
-    
+
+$page = '';
+
+if (!empty(htmlspecialchars($_GET["page"]))) {
     $page = htmlspecialchars($_GET["page"]);
-    
-     if (empty($page)) {
-                include '/page/home.php';
-            } else {
-                if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/page/' . $page . '.php')) {
-                    include $_SERVER['DOCUMENT_ROOT'] . '/page/' . $page . '.php';
-                } else {
-                    include '/error/404.php';
-                }
-            }
-    
+}
+
+
+kint::dump($page);
+
+if (empty($page)) {
+    include '/page/home.php';
+}
+else {
+    if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/page/' . $page . '.php')) {
+        include $_SERVER['DOCUMENT_ROOT'] . '/page/' . $page . '.php';
+    }
+    else {
+        include '/error/404.php';
+    }
+}
 ?>
