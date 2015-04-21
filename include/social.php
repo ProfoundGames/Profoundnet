87eb2061437dc8e07b8e96f37212a0073f9480ac
ianno1993 <i.lol@hotmail.com>
2015-04-21 17:02:33 +0200

    Social Styling

--- include/social.php
+++ include/social.php
@@ -1,9 +1,9 @@
 <div class="social" style="position: fixed;">
           <ul>
               <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-              <li><a href="http://profoundnet.com/s/ProfoundGamesYT/" class="red-text">Youtube <i class="fa fa-youtube"></i></a></li>
-              <li><a href="http://profoundnet.com/s/ProfoundGamesTW/" class="purple-text">Twitch <i class="fa fa-twitch"></i></a></li>
-              <li><a href="http://profoundnet.com/s/ProfoundGamesT/" class="blue-text lighten-1">Twitter <i class="fa fa-twitter"></i></a></li>
-              <li><a href="http://profoundnet.com/s/ProfoundGamesF/" class="indigo-text darken-4">Facebook <i class="fa fa-facebook"></i></a></li>
+              <li class="z-depth-2 waves-effect waves-red"><a href="http://profoundnet.com/s/ProfoundGamesYT/" class="red-text">Youtube <i class="fa fa-youtube"></i></a></li>
+              <li class="z-depth-2 waves-effect waves-purple"><a href="http://profoundnet.com/s/ProfoundGamesTW/" class="purple-text">Twitch <i class="fa fa-twitch"></i></a></li>
+              <li class="z-depth-2 waves-effect waves-blue"><a href="http://profoundnet.com/s/ProfoundGamesT/" class="blue-text lighten-1">Twitter <i class="fa fa-twitter"></i></a></li>
+              <li class="z-depth-2 waves-effect waves-indigo"><a href="http://profoundnet.com/s/ProfoundGamesF/" class="indigo-text darken-4">Facebook <i class="fa fa-facebook"></i></a></li>
           </ul>
       </div>
