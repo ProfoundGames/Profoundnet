a4c053855cb8834dde9ca024093a05f3c1e6ba1c
ianno1993 <i.lol@hotmail.com>
2015-05-01 17:06:22 +0200

    Goto Youtube buton

--- include/youtubeInfo.php
+++ include/youtubeInfo.php
@@ -3,6 +3,7 @@
     include '/settings.php';
     
     $channelInfo = file_get_contents('https://www.googleapis.com/youtube/v3/channels?part=contentDetails&id='. $settings[youtubeInfo][channelID] .'&maxResults=15&key=' . $settings[youtubeInfo][devKey]);
+    //var_dump($channelInfo);
     $channelInfo = json_decode($channelInfo);
     $channelUploadID = $channelInfo->items[0]->contentDetails->relatedPlaylists->uploads;
     
@@ -13,10 +14,16 @@
     $pageContent;
     
     $pageContent .= '<script>
+                        $(document).on("pagecontainerload",function(){
+                            alert("pagecontainerload event fired!");
+                        });
                         $(document).ready(function () {
                             $(".page-footer").css({ "margin-top": "0px"});
+                            $(".tooltipped").tooltip({delay: 50});
                         });
-                        </script>';
+                        </script>'
+                        
+            ;
     
     foreach ($channelUploadItems as $value) {
     
@@ -24,7 +31,8 @@
                         <div class="section white youtube z-depth-2 '. $value->snippet->resourceId->videoId .'">
                             <div class="row container youtubeContainer" style="width: 100%; max-width: none;">
                                 <h3 class="header" style="height: auto !important;">'. $value->snippet->title .'</h3>
-                               <div class="videoWrapper">
+                                <a class="waves-effect waves-light btn tooltipped" data-position="left" data-delay="50" data-tooltip="Open on Youtube" href="https://www.youtube.com/watch?v='. $value->snippet->resourceId->videoId .'">Youtube</a>
+                                <div class="videoWrapper">
                                     <iframe class="YoutubePlayer YoutubePlayer'. $value->snippet->resourceId->videoId .'" width="1920" height="1080" src="http://www.youtube.com/embed/'. $value->snippet->resourceId->videoId .'?controls=2" frameborder="0" allowfullscreen controls=0></iframe>
                                 </div>
                             </div>
@@ -49,7 +57,7 @@
 ';        
 }
     
-    //var_dump($settings[youtubeInfo][channelID]);
+    
     
 ?>
 
