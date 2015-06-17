b598501c8fc37de072cd0adbb970ecf6d018e0f6
Profound Games <i.wijma@ziggo.nl>
2015-06-17 20:02:58 +0200

    PageControllerFix

--- function/pageController.php
+++ function/pageController.php
@@ -4,13 +4,15 @@
     
      if (empty($page)) {
                 include '/page/home.php';
-            }elseif ($page == home){
+            }elseif ($page == 'home'){
                 header('Location: /');
-            }elseif ($page == i){
+            }elseif ($page == 'i'){
                 header('Location: /i/index.php');
+            }elseif ($page == 'pdfGen'){
+                header('Location: /pdfGen/index.php');
             } else {
-                if (file_exists($_SERVER['DOCUMENT_ROOT'] . '/page/' . $page . '.php')) {
-                    include $_SERVER['DOCUMENT_ROOT'] . '/page/' . $page . '.php';
+                if (file_exists(filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/page/' . $page . '.php')) {
+                    include filter_input(INPUT_SERVER, 'DOCUMENT_ROOT') . '/page/' . $page . '.php';
                 } else {
                     include '/error/404.php';
                 }
