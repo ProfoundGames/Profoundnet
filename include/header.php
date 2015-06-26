085f89cc297357fc3f830bca66c5d3fdfc784abd
Profound Games <i.wijma@ziggo.nl>
2015-06-26 14:01:31 +0200

    Small edits where made that day

--- include/header.php
+++ include/header.php
@@ -5,15 +5,19 @@
             
             
             <div class="col s12 m6 meImg right-align">
-                <img src="/images/me.png" alt=""/>
+                <img src="/images/me.png" alt="" class="z-depth-2"/>
             </div>
             
             <div class="col s12 m6 meTxt left-align">
+                <blockquote>
+                    <br>
                 <h1>
                     Welcome
                 </h1>
 
                 <p>My name is Ian Wijma (aka Profound Games) <br> and I'm an awesome dude from the internet.</p>
+                <br>
+                </blockquote>
             </div>
             
             
